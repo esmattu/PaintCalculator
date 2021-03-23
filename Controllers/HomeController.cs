c678c0e21c52090694a0ecfdf03d890366671fcf
Eishpal Mattu <eish007@hotmail.com>
2021-03-23 00:02:42 +0000

    Created a simple input form to take the room values. With a room just having four walls and the height of the room.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,13 @@ namespace PaintCalculator.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public IActionResult Results()
+        {
+
+            return View();
+        }
+
     }
 }
---
+++ Models/RoomMeasurements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaintCalculator.Models
+{
+    public class RoomMeasurements
+    {
+        [Display(Name = "Wall One")]
+        [Range(1,100)]
+        [Required]
+        public int wallOne { get; set; }
+        [Display(Name = "Wall Two")]
+        [Range(1, 100)]
+        [Required]
+        public int wallTwo { get; set; }
+        [Display(Name = "Wall Three")]
+        [Range(1, 100)]
+        [Required]
+        public int wallThree { get; set; }
+        [Display(Name = "Wall Four")]
+        [Range(1, 100)]
+        [Required]
+        public int wallFour { get; set; }
+        [Display(Name = "Room Height")]
+        [Range(1, 100)]
+        [Required]
+        public int roomHeight { get; set; }
+    }
+}
