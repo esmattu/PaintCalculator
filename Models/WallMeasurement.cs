1215d025d8b99dbff46550cffb86a5b078ec3e10
Eishpal Mattu <eish007@hotmail.com>
2021-03-29 00:29:42 +0100

    Changed the approach to allow the user to add multiple walls. I won't be using the WallMeasurement.cs model. Index.cshtml will now allow the user to add multiple walls for a room and validate that the inputs have a value. I have also added a fontawesome and sweetalert packages to the lib folder

---
+++ Models/WallMeasurement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+ * This class will store the wall height and width
+ */
+
+namespace PaintCalculator.Models
+{
+    public class WallMeasurement
+    {
+        [Display(Name = "Wall Height")]
+        [Range(1,100)]
+        [Required]
+        public int WallHeight { get; set; }
+        [Display(Name = "Wall Width")]
+        [Range(1, 100)]
+        [Required]
+        public int WallWidth { get; set; }
+
+    }
+}
